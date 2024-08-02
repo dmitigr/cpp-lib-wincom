99adcaf02395497fa16758c237cc1cc02b7d3fd5
Dmitry Igrishin <dmitigr@gmail.com>
2024-08-02 20:22:36 +0300

    Add initialize_security() and set_proxy_blanket()

--- library.hpp
+++ library.hpp
@@ -15,6 +15,7 @@
 // limitations under the License.
 
 #pragma once
+#pragma comment(lib, "ole32")
 
 #include "../base/noncopymove.hpp"
 #include "../winbase/windows.hpp"
@@ -42,5 +43,53 @@ public:
       throw Win_error{"cannot initialize COM library", err};
   }
 };
+
+/**
+ * @param auth A value of `-1` tells COM to choose authentication services
+ * to register.
+ * @param auth_services Must be `nullptr` if `auth` is set to `-1`.
+ */
+inline void initialize_security(const PSECURITY_DESCRIPTOR sec_desc = {},
+  const DWORD auth = -1,
+  SOLE_AUTHENTICATION_SERVICE* const auth_services = {},
+  const DWORD auth_level = RPC_C_AUTHN_LEVEL_DEFAULT,
+  const DWORD imperson_level = RPC_C_IMP_LEVEL_DEFAULT,
+  SOLE_AUTHENTICATION_LIST* const auth_list = {},
+  const DWORD capabilities = EOAC_DEFAULT)
+{
+  const auto err = CoInitializeSecurity(sec_desc, auth, auth_services,
+    nullptr/*reserved*/, auth_level, imperson_level, auth_list, capabilities,
+    nullptr/*reserved*/);
+  if (err == RPC_E_TOO_LATE)
+    return;
+  else
+    throw_if_error(err, "cannot register security and set the default security"
+      " values for the process");
+}
+
+/**
+ * @param proxy The proxy to set.
+ * @param authn Authentication service.
+ * @param authz Authorization service.
+ * @param server_princ_name Server principal name.
+ * @param authn_level Authentication level.
+ * @param imperson_level Impersonation level.
+ * @param auth_info Client identity.
+ * @param capabilities Proxy capabilities.
+ */
+inline void set_proxy_blanket(IUnknown* const proxy,
+  const DWORD authn = RPC_C_AUTHN_DEFAULT,
+  const DWORD authz = RPC_C_AUTHZ_DEFAULT,
+  OLECHAR* const server_princ_name = nullptr,
+  const DWORD authn_level = RPC_C_AUTHN_LEVEL_DEFAULT,
+  const DWORD imperson_level = RPC_C_IMP_LEVEL_DEFAULT,
+  const RPC_AUTH_IDENTITY_HANDLE auth_info = nullptr,
+  const DWORD capabilities = EOAC_DEFAULT)
+{
+  const auto err = CoSetProxyBlanket(proxy, authn, authz, server_princ_name,
+    authn_level, imperson_level, auth_info, capabilities);
+  throw_if_error(err, "cannot sets the authentication info that will be used"
+    " to make calls on the specified proxy");
+}
 
 } // namespace dmitigr::wincom
